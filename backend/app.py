6d6461645ac74e3aa29cb33fe05f7404506f8ce8
Mark Allison <markallison2001@hotmail.com>
2024-04-19 23:33:02 +0100

    working on running flask and angular through docker

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/data')
 def get_data():
-    return jsonify({"message": "Hello this is served from flask!"})
+    return jsonify({"message": "Hello this is served from flask! now running through docker!!!!"})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5100)
+    app.run(host='0.0.0.0', debug=True, port=5100)
