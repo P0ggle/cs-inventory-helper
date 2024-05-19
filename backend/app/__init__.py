540120c4075944a23b5ecee945fe181e1557ff93
Mark Allison <markallison2001@hotmail.com>
2024-05-19 15:49:29 +0100

    Implemented MongoDB into the project
    -mongo db into __init__.py
    -added some dummy data to just test
    -some routes for testing the database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 
 from .routes import setup_routes
 
@@ -7,5 +8,27 @@ from .routes import setup_routes
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    setup_routes(app)
+
+    app.config["MONGO_URI"] = (
+        "mongodb://mongoadmin:secret@mongo:27017/admin?authSource=admin"
+    )
+    mongo = PyMongo(app)
+
+    setup_routes(app, mongo)
+    add_dummy_data(mongo.db)
+
     return app
+
+
+def add_dummy_data(db):
+    users_collection = db.users
+
+    if users_collection.count_documents({}) == 0:
+        dummy_users = [
+            {"name": "Alice", "age": 30},
+            {"name": "Bob", "age": 25},
+            {"name": "Charlie", "age": 35},
+            {"name": "Mark", "age": 22},
+        ]
+
+        users_collection.insert_many(dummy_users)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,24 @@
-from flask import jsonify
+from flask import Blueprint, jsonify
 
 from .services import extract_data
 
 
-def setup_routes(app):
-    @app.route("/api/data", methods=["GET"])
+def setup_routes(app, mongo):
+    main = Blueprint("main", __name__)
+
+    @main.route("/api/data", methods=["GET"])
     def get_data():
         extracted_data = extract_data()
         return jsonify(message=extracted_data)
+
+    @main.route("/")
+    def home():
+        return jsonify({"message": "Welcome to the Flask app with MongoDB!"})
+
+    @main.route("/users")
+    def get_users():
+        users = mongo.db.users.find()
+        users_list = [{"name": user["name"], "age": user["age"]} for user in users]
+        return jsonify(users_list)
+
+    app.register_blueprint(main)
