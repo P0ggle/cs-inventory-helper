8e10f291836272bcc7721d2ba393eba5d0ed7c60
Mark Allison <markallison2001@hotmail.com>
2024-06-17 22:19:00 +0100

    adding first route using mongo db

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,4 +8,14 @@ def setup_routes(app, mongo):
     def get_data():
         return jsonify({"temp": "temp"})
 
+    @app.route("/api/cs-skins/first", methods=["GET"])
+    def get_first_cs_skin():
+        collection = mongo.db["cs-skins"]
+        first_item = collection.find_one()
+        if first_item:
+            first_item["_id"] = str(first_item["_id"])
+            return jsonify(first_item)
+        else:
+            return (jsonify({"error": "No items found in the collection"}),)
+
     app.register_blueprint(main)
