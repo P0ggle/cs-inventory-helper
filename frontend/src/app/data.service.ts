3643043f5025c70c20923a72cf3492fa84055942
Mark Allison <markallison2001@hotmail.com>
2024-06-20 23:58:47 +0100

    adding in the correct route for testing

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class DataService {
-  private backendUrl = "http://backend:5100/api/data";
+  private backendUrl = 'http://backend:5100/api/cs-skins/first';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   fetchData(): Observable<{ message: string }> {
     return this.http.get<{ message: string }>(this.backendUrl);
